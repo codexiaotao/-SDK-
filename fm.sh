125f016eae0d31ef93f8171218395cb98558fb0f
codexiaotao <51529384+codexiaotao@users.noreply.github.com>
2020-11-28 15:56:48 +0800

    Update fm.sh

--- fm.sh
+++ fm.sh
@@ -5,4 +5,4 @@ cd /home/pi/myDocument/kdxf/bin
 python3 /home/pi/myPySpace/QHDWeather.py | xargs ./tts_sample
 cp -l weather.wav /home/pi/fm/fm_transmitter
 cd /home/pi/fm/fm_transmitter
-sox acoustic_guitar_duet.wav -r 22050 -c 1 -b 16 -t wav - | sudo ./fm_transmitter -f 100.6 -
+sox weather.wav -r 22050 -c 1 -b 16 -t wav - | sudo ./fm_transmitter -f 100.6 -
